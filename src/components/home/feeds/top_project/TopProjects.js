14c1af31301427b75fc9f964737c86c98c17ee01
John <johnnonso090@gmail.com>
2022-01-05 10:51:17 +0100

    created the top project component to display recent project with best votes

--- src/components/home/feeds/top_project/TopProjects.js
+++ src/components/home/feeds/top_project/TopProjects.js
@@ -1,16 +1,64 @@
-import { Box, List, ListSubheader, Menu, MenuItem, Paper, Typography } from '@mui/material'
-import React from 'react'
-import TopProjectsLoader from './TopProjectsLoader'
+import { Avatar, Badge, Box, List, ListItem, ListItemButton, ListItemIcon, ListSubheader, Menu, MenuItem, Paper, Stack, Typography } from '@mui/material'
+import { deepOrange } from '@mui/material/colors'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function TopProjects() {
 
+    const [projects, setProjects] = useState([{id: 2, title: "Todo Application", desc: "A Todo dfsfsdsfsfsfsfsdfsfsappl"},
+     {id: 2, title: "Todo Application", desc: "A Todo applsdfsfsdfsdf"}, {id: 2, title: "Todo Application", desc: "A Todo sdfssfsfsdfsdffsfsdfsdfsappl"},
+     {id: 2, title: "Todo Application", desc: "A Todo applsdfsdfsdfs"},
+     {id: 2, title: "Todo Application", desc: "A Todo appsdfsl"}
+    ])
+
+
+    function Projects({projects}) {
+
+        return (
+            <>
+                {
+                    projects.map(project => (
+                        <ListItem key={project.id} divider>
+                            <Link to="/xpo" style={{textDecoration: "none", width: "100%"}} >
+
+                            
+                                <Box display="flex" width="100%" alignItems="center" >
+                                    
+                                    <ListItemIcon>
+                                        
+                                        <Badge badgeContent={25}  color="primary" >
+                                            <img width="32"  src="/images/portfolio.png" alt="portfolio" />
+                                        </Badge>
+                                    </ListItemIcon>
+                                    <Stack width="80%" >
+                                        <Typography zer color="ButtonShadow" variant="body1"> {project.title}</Typography>
+                                        <Typography variant="body2" color="ButtonShadow" noWrap={true}> {project.desc}</Typography>
+                                        
+                                    
+                                    </Stack>
+                                </Box>
+                            </Link>
+
+                        </ListItem>
+                    ))
+                }
+    
+            </>
+            
+        )
+    
+    }
 
     return (
         
-            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', borderRadius: "10px" }} component="nav" 
+            <List sx={{ width: '100%', maxWidth: 360, minHeight: 400, bgcolor: 'background.paper', borderRadius: "10px" }} component="nav" 
                 subheader={<ListSubheader  component="div" sx={{textAlign: "left"}} > Top Projects </ListSubheader>}  >
-                <TopProjectsLoader />
+                 <Projects projects={projects} />
             </List>
      
     )
 }
+
+
+

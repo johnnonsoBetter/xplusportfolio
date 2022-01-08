8d2719e4c0b11b867c37241395da9ed3a8fb0ded
John <johnnonso090@gmail.com>
2022-01-08 12:16:38 +0100

    Created the list of item that the  mobile search will render "

--- src/components/home/mobile/SearchMenu.js
+++ src/components/home/mobile/SearchMenu.js
@@ -1,13 +1,14 @@
-import { Box, Button } from '@mui/material'
+import { Avatar, Box, Button, List, ListItem, ListItemIcon, Stack, Typography } from '@mui/material'
 import React, { useContext } from 'react'
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
-import { PsychologyRounded } from '@mui/icons-material';
-import { blueGrey, grey } from '@mui/material/colors';
+import { Folder, PsychologyOutlined, PsychologyRounded } from '@mui/icons-material';
+import { blueGrey, grey, purple, red } from '@mui/material/colors';
 import DrawerContext from '../../../context/DrawerContext';
+import { Link } from 'react-router-dom';
 
  function Search() {
   return (
@@ -19,7 +20,7 @@ import DrawerContext from '../../../context/DrawerContext';
         sx={{ ml: 1, flex: 1}}
         placeholder="Search "
         inputProps={{ 'aria-label': 'search google maps' }}
-
+        size="small"
       />
       <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
         <SearchIcon />
@@ -43,9 +44,111 @@ export default function MobileSearch() {
                 <Button onClick={closeDrawer} disableRipple size="small" color="inherit" sx={{color: grey[600]}} >
                     Cancel
                 </Button>
-            
-     
             </Box>
+
+            <List sx={{my: 2}} >
+                <ListItem  >
+                    <Link to={`/xpo/projects/${4}`} style={{textDecoration: "none", width: "100%"}} >
+
+                        <Box display="flex" width="100%" alignItems="center" >
+                            
+                            <ListItemIcon>
+                            <Avatar sx={{backgroundColor: purple[300]}} > <Folder  /> </Avatar>
+                                
+                            </ListItemIcon>
+                            <Stack  >
+                                
+                                
+                                <Box  maxWidth="90" >
+                                    <Typography sx={{textTransform: "capitalize"}} color="ButtonText" variant="body1"> Todo application</Typography>
+                                </Box>
+                                <Box maxWidth="95%" >
+                                <Typography  variant="body2" color="ButtonShadow" noWrap={true}> A todo list application that helps "</Typography>
+                                </Box>
+                            
+                            </Stack>
+                        </Box>
+                    </Link>
+
+                </ListItem>
+
+                <ListItem  >
+                    <Link to={`/xpo/projects/${4}`} style={{textDecoration: "none", width: "100%"}} >
+
+                        <Box display="flex" width="100%" alignItems="center" >
+                            
+                            <ListItemIcon>
+                            <Avatar sx={{backgroundColor: purple[300]}} > <Folder  /> </Avatar>
+                                
+                            </ListItemIcon>
+                            <Stack  >
+                                
+                                
+                                <Box  maxWidth="90" >
+                                    <Typography sx={{textTransform: "capitalize"}} color="ButtonText" variant="body1"> Todo application</Typography>
+                                </Box>
+                                <Box maxWidth="95%" >
+                                <Typography  variant="body2" color="ButtonShadow" noWrap={true}> A todo list application that helps "</Typography>
+                                </Box>
+                            
+                            </Stack>
+                        </Box>
+                    </Link>
+
+                </ListItem>
+
+                <ListItem  >
+                    <Link to={`/xpo/projects/${4}`} style={{textDecoration: "none", width: "100%"}} >
+
+                        <Box display="flex" width="100%" alignItems="center" >
+                            
+                            <ListItemIcon>
+                            <Avatar src="/images/selfie.jpeg" sx={{backgroundColor: purple[200]}} />
+                                
+                            </ListItemIcon>
+                            <Stack  >
+                                
+                                
+                                <Box maxWidth="90" >
+                                    <Typography sx={{textTransform: "capitalize"}} color="ButtonText" variant="body1"> Chukwuma obi kemi</Typography>
+                                </Box>
+                                <Box maxWidth="95%" >
+                                <Typography  variant="body2" color="ButtonShadow" noWrap={true}>  A Ruby on rails developer </Typography>
+                                </Box>
+                            
+                            </Stack>
+                        </Box>
+                    </Link>
+
+                </ListItem>
+
+                <ListItem  >
+                    <Link to={`/xpo/projects/${4}`} style={{textDecoration: "none", width: "100%"}} >
+
+                        <Box display="flex" width="100%" alignItems="center" >
+                            
+                            <ListItemIcon>
+                            <Avatar sx={{backgroundColor: red[300]}} > <PsychologyOutlined  /> </Avatar>
+                                
+                            </ListItemIcon>
+                            <Stack  >
+                                
+                                
+                                <Box  maxWidth="90" >
+                                    <Typography sx={{textTransform: "capitalize"}} color="ButtonText" variant="body1"> Todo application</Typography>
+                                </Box>
+                                <Box maxWidth="95%" >
+                                <Typography  variant="body2" color="ButtonShadow" noWrap={true}> A todo list application that helps "</Typography>
+                                </Box>
+                            
+                            </Stack>
+                        </Box>
+                    </Link>
+
+                </ListItem>
+
+                
+            </List>
             
         </Box>
     )

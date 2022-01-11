9d5ad6b5658d8cb47a8d8332928210d285384777
John <johnnonso090@gmail.com>
2022-01-11 12:14:45 +0100

    the search list renders list of users, projects

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -1,33 +1,184 @@
-
-
-import React from 'react';
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { Avatar, IconButton, List, ListItem, ListItemIcon, Paper, Stack, Typography } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import { PsychologyRounded } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { Folder, PsychologyOutlined } from '@mui/icons-material';
+import { purple, red } from '@mui/material/colors';
+
 
 export default function Search() {
   return (
-    <Paper
-      component="form"
-      sx={{ p: '2px 4px', display: {xs: 'none', sm: 'flex'}, mr: 2, alignItems: 'center', width: "60%" }}
-    >
-    
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search "
-        inputProps={{ 'aria-label': 'search google maps' }}
-      />
-      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-        <PsychologyRounded />
-      </IconButton>
+    <Autocomplete
+      id="country-select-demo"
+      sx={{ width: "50%", display: {xs: 'none', sm: 'block'}}}
+      size="small"
+      options={countries}
+      autoHighlight
       
-    </Paper>
+      getOptionLabel={(option) => option.label}
+      renderOption={(props, option) => (
+        <SearchList />
+      )}
+      renderInput={(params) => (
+        <Paper
+        elevation={2}
+        sx={{ p: '1px 3px', display: 'flex', alignItems: 'center' }}
+        >
+          <TextField
+          {...params}
+          
+          variant="standard"
+          fullWidth
+          size="small"
+          label="  Search"
+          
+        />
+       
+        </Paper>
+        
+      )}
+    />
   );
 }
+
+
+
+const SearchList = () => {
+
+  return (
+    <List sx={{my: 2}} >
+                <ListItem  >
+                    <Link to={`/xpo/projects/${4}`} style={{textDecoration: "none", width: "100%"}} >
+
+                        <Box display="flex" width="100%" alignItems="center" >
+                            
+                            <ListItemIcon>
+                            <Avatar sx={{backgroundColor: purple[300]}} > <Folder /> </Avatar>
+                                
+                            </ListItemIcon>
+                            <Stack  >
+                                
+                                
+                                <Box  maxWidth="90" >
+                                    <Typography variant="body2" sx={{textTransform: "capitalize"}} color="ButtonText"> Todo application</Typography>
+                                </Box>
+                                <Box maxWidth="95%" >
+                                <Typography  variant="body2" color="ButtonShadow" noWrap={true}> A todo list application that helps "</Typography>
+                                </Box>
+                            
+                            </Stack>
+                        </Box>
+                    </Link>
+
+                </ListItem>
+
+                <ListItem  >
+                    <Link to={`/xpo/projects/${4}`} style={{textDecoration: "none", width: "100%"}} >
+
+                        <Box display="flex" width="100%" alignItems="center" >
+                            
+                            <ListItemIcon>
+                            <Avatar sx={{backgroundColor: purple[300]}} > <Folder  /> </Avatar>
+                                
+                            </ListItemIcon>
+                            <Stack  >
+                                
+                                
+                                <Box  maxWidth="90" >
+                                    <Typography sx={{textTransform: "capitalize"}} color="ButtonText" variant="body2"> Todo application</Typography>
+                                </Box>
+                                <Box maxWidth="95%" >
+                                <Typography  variant="body2" color="ButtonShadow" noWrap={true}> A todo list application that helps "</Typography>
+                                </Box>
+                            
+                            </Stack>
+                        </Box>
+                    </Link>
+
+                </ListItem>
+
+                <ListItem  >
+                    <Link to={`/xpo/projects/${4}`} style={{textDecoration: "none", width: "100%"}} >
+
+                        <Box display="flex" width="100%" alignItems="center" >
+                            
+                            <ListItemIcon>
+                            <Avatar src="/images/selfie.jpeg" sx={{backgroundColor: purple[200]}} />
+                                
+                            </ListItemIcon>
+                            <Stack  >
+                                
+                                
+                                <Box maxWidth="90" >
+                                    <Typography sx={{textTransform: "capitalize"}} color="ButtonText" variant="body2"> Chukwuma obi kemi</Typography>
+                                </Box>
+                                <Box maxWidth="95%" >
+                                <Typography  variant="body2" color="ButtonShadow" noWrap={true}>  A Ruby on rails developer </Typography>
+                                </Box>
+                            
+                            </Stack>
+                        </Box>
+                    </Link>
+
+                </ListItem>
+
+                <ListItem  >
+                    <Link to={`/xpo/projects/${4}`} style={{textDecoration: "none", width: "100%"}} >
+
+                        <Box display="flex" width="100%" alignItems="center" >
+                            
+                            <ListItemIcon>
+                            <Avatar sx={{backgroundColor: red[300]}} > <PsychologyOutlined  /> </Avatar>
+                                
+                            </ListItemIcon>
+                            <Stack  >
+                                
+                                
+                                <Box  maxWidth="90" >
+                                    <Typography sx={{textTransform: "capitalize"}} color="ButtonText" variant="body2"> Todo application</Typography>
+                                </Box>
+                                <Box maxWidth="95%" >
+                                <Typography  variant="body2" color="ButtonShadow" noWrap={true}> A todo list application that helps "</Typography>
+                                </Box>
+                            
+                            </Stack>
+                        </Box>
+                    </Link>
+
+                </ListItem>
+
+                
+            </List>
+  )
+}
+
+
+
+// From https://bitbucket.org/atlassian/atlaskit-mk-2/raw/4ad0e56649c3e6c973e226b7efaeb28cb240ccb0/packages/core/select/src/data/countries.js
+const countries = [
+  { code: 'AD', label: 'Andorra', phone: '376' },
+  {
+    code: 'AE',
+    label: 'United Arab Emirates',
+    phone: '971',
+  },
+  { code: 'AF', label: 'Afghanistan', phone: '93' },
+  {
+    code: 'AG',
+    label: 'Antigua and Barbuda',
+    phone: '1-268',
+  },
+  { code: 'VN', label: 'Vietnam', phone: '84' },
+  { code: 'VU', label: 'Vanuatu', phone: '678' },
+  { code: 'WF', label: 'Wallis and Futuna', phone: '681' },
+  { code: 'WS', label: 'Samoa', phone: '685' },
+  { code: 'XK', label: 'Kosovo', phone: '383' },
+  { code: 'YE', label: 'Yemen', phone: '967' },
+  { code: 'YT', label: 'Mayotte', phone: '262' },
+  { code: 'ZA', label: 'South Africa', phone: '27' },
+  { code: 'ZM', label: 'Zambia', phone: '260' },
+  { code: 'ZW', label: 'Zimbabwe', phone: '263' },
+];

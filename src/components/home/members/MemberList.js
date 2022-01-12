7fe8cbd827ea754ed8ce06338248508cae0ed473
John <johnnonso090@gmail.com>
2022-01-12 17:48:10 +0100

    fixed some layout issues

--- src/components/home/members/MemberList.js
+++ src/components/home/members/MemberList.js
@@ -7,7 +7,7 @@ export default function MemberList() {
 
 
     return (
-        <Box sx={{width: "100%",  height: {sm: "calc(99vh - 60px)", xs: "calc(96vh - 85px)"}, scrollbarWidth: {display: "none"}, overflowY: "auto"}} >
+        <Box sx={{width: "100%", scrollbarColor: "red",  height: {sm: "calc(99vh - 60px)", xs: "calc(96vh - 85px)"}, scrollbarWidth: {display: "none"}, overflowY: "auto"}} >
            <Grid container spacing={1} >
              
                 <Grid item xs={6} sm={4} md={4}  >

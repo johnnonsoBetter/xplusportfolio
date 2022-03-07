47ad7746c3f6d201c7bd2740c13d2390554f0c32
John Nonso <johnnonso090@gmail.com>
2022-03-07 17:01:29 +0100

    fixed the url to fetch data

--- src/context/FetchContext.js
+++ src/context/FetchContext.js
@@ -8,7 +8,7 @@ const {Provider} = FetchContext
 const FetchProvider = ({children}) => {
     
     const authAxios = axios.create({
-        baseURL: process.env.NODE_ENV === 'development'? 'http://localhost:3001' : 'http://localhost:3001'
+        baseURL: process.env.NODE_ENV === 'development'? 'http://localhost:3001' : 'https://xplusportfolio.herokuapp.com'
     })
 
     const {isAuthenticated, setAuthState, setSomethingWentWrong} = useContext(AuthContext)

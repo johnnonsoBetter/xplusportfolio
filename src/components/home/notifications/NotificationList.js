10020c219b8b44aba9bd183c5cbfca994bf7fb7c
John Nonso <johnnonso090@gmail.com>
2022-03-17 18:32:38 +0100

    fixed some bugs on the notification system

--- src/components/home/notifications/NotificationList.js
+++ src/components/home/notifications/NotificationList.js
@@ -1,5 +1,6 @@
 import { FolderRounded, HowToVoteRounded, PsychologyRounded, ThumbUpRounded } from '@mui/icons-material'
 import { Avatar, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material'
+import { orange, purple } from '@mui/material/colors'
 import { Box } from '@mui/system'
 import React, { useContext } from 'react'
 import { FetchContext } from '../../../context/FetchContext'
@@ -57,15 +58,15 @@ const Notification = ({object, type,  user_slug}) => {
         <>
         {
             type === "AnticipationLikeNotification" ?
-            <NotificationInfo  body={object['anticipation'].body} link={`/xpo/my_profile/${user_slug}/anticipations/`} title="Anticipation Like" icon={<ThumbUpRounded   fontSize='0.7rem' />}/>
+            <NotificationInfo  body={object['anticipation'].body} link={`/xpo/my_profile/${user_slug}/anticipations/`} title="Anticipation Like" backgroundColor={purple[400]} icon={<ThumbUpRounded   fontSize='0.7rem' />}/>
             : 
             type === "UpvoteNotification" ? 
-            <NotificationInfo  body={object['project'].title} link={`/xpo/projects/${object['project'].slug}`} title="Project Upvote" icon={ <HowToVoteRounded   fontSize='0.7rem' />}/> 
+            <NotificationInfo  body={object['project'].title} link={`/xpo/projects/${object['project'].slug}`} title="Project Upvote" backgroundColor={orange[400]} icon={ <HowToVoteRounded   fontSize='0.7rem' />}/> 
             :
             type === "NewAnticipationNotification" ?
-            <NotificationInfo  body={object['anticipation'].body} link={`/xpo/my_profile/${user_slug}/anticipations/`} title="New Anticipation" icon={ <PsychologyRounded   fontSize='0.7rem' />}/> : 
+            <NotificationInfo  body={object['anticipation'].body} link={`/xpo/my_profile/${user_slug}/anticipations/`} title="New Anticipation" backgroundColor={purple[400]} icon={ <PsychologyRounded   fontSize='0.7rem' />}/> : 
             type === "ProjectLikeNotification" ? 
-            <NotificationInfo  body={object['project'].title} link={`/xpo/projects/${object['project'].slug}`} title="Project Like" icon={ <ThumbUpRounded   fontSize='0.7rem' />}/> 
+            <NotificationInfo  body={object['project'].title} link={`/xpo/projects/${object['project'].slug}`} title="Project Like" backgroundColor={orange[400]} icon={ <ThumbUpRounded   fontSize='0.7rem' />}/> 
             : null
         }
     </>

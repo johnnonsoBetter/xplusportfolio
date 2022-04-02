c83ed2484740e5f3bff024a0bb162bd35a905f24
John Nonso <johnnonso090@gmail.com>
2022-04-02 13:15:23 +0100

    fixed the confirm_success_url for the signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -62,7 +62,7 @@ export default function Signup(){
         name: '',
         github_url: null,
         avatar_url: null,
-        confirm_success_url: 'https://xplusportfoliob.herokuapp.com'
+        confirm_success_url: 'https://xplusportfolio.tech'
 
       },
       validationSchema: validationSchema,
